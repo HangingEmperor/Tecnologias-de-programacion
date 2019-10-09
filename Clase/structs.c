a49d02e04e74146f739f2c93f46ded22ce7da28f
Omar Flores Salazar <31521501+HangingEmperor@users.noreply.github.com>
2019-10-09 15:32:09 -0700

    Create structs.c

---
+++ Clase/structs.c
@@ -0,0 +1,61 @@
+/*
+Hacer uso de struct, apuntador flecha
+struct cd* disco1
+scanf("%d", &disco1->ID)
+*/
+#include <stdio.h>
+
+struct cd
+{
+    int ID;
+    char artista[50];
+    char disco[50];
+    int numCancion;
+    float tiempo;
+    float precio;
+};
+
+void imprimir(struct cd discos[5]);
+void leer(struct cd discos[5], int size);
+
+int main()
+{
+    int size = 0;
+    struct cd discos[5];
+
+    do
+    {
+        leer(discos, size);
+        size++;
+    } while (size != 5);
+
+    int i;
+    printf("Agregados: \n");
+    imprimir(discos);
+
+    return 0;
+}
+
+void imprimir(struct cd discos[5])
+{
+    int i;
+    for (i = 0; i < 5; i++)
+    {
+        printf(" ------------- \n");
+        printf("ID (%i): %i \n", i, discos[i].ID);
+        printf("Nombre (%i): %s\n", i, discos[i].artista);
+        printf(" ------------- \n");
+    }
+}
+void leer(struct cd discos[5], int size)
+{
+    printf("ID (%i): ", size + 1);
+    scanf("%i", &discos[size].ID);
+    while (getchar() != '\n')
+        ;
+
+    printf("Nombre (%i): ", size + 1);
+    scanf("%s", discos[size].artista);
+    while (getchar() != '\n')
+        ;
+}
