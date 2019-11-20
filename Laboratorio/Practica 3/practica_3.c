17f81ea800fbd8412dc9f4683332bbb284efe748
Omar Flores Salazar <31521501+hangingemperor@users.noreply.github.com>
2019-11-20 00:14:08 -0600

    Agregados a Practica 3.

--- Laboratorio/Practica 3/practica_3.c
+++ Laboratorio/Practica 3/practica_3.c
@@ -13,8 +13,8 @@ void mostrarTablero(char tablero[12][12], int filas, int columnas);
 void reiniciarTablero(char tablero[12][12]);
 
 void generarCoordenadas(char tablero[12][12], int filas, int columnas, int size,
-                        int barcosTipo[], char coordenadasInicio[12][1],
-                        char orientacion[12]);
+                        int barcosTipo[10], char coordenadasInicio[10][1],
+                        char orientacion[10]);
 
 void mostrarHUD(char nombre[], char coordFalladas, char coordAtinadasm,
                 int barcosHundidos, int barcosRestantes);
@@ -22,9 +22,15 @@ void mostrarHUD(char nombre[], char coordFalladas, char coordAtinadasm,
 void dibujarBarcos(char tablero[12][12], int filas, int columnas, int barcosTipos[12],
                    char coordenadasInicio[12][1], char orientacion[12]);
 
+void generarOrientacionesAleatorias(char orientacion[10], int size);
+
+void generarCoordenadasAleatorias(char coordenadaSend[10][1], int barcosTipos[10],
+                                  char orientacion[10], int size);
+
 int main(int argc, char *argv[]) {
     if (argc == 3) {
         if (argv[1][0] >= '5' && argv[1][0] <= '9' || ((int) argv[0][1] - 48) == 10) {
+            srand(time(NULL));
             int total;
             int buquesCarga;
             int buquesBatalla;
@@ -67,8 +73,8 @@ int main(int argc, char *argv[]) {
             printf("Barcos restantes: \n");
             printf("COORDENADA: ");
             scanf("%s", coordenada);
-            coordenadaSend[0][0] = coordenada[0];
-            coordenadaSend[0][1] = coordenada[2];
+            //coordenadaSend[0][0] = coordenada[0];
+            //coordenadaSend[0][1] = coordenada[2];
 
             int bB = buquesBatalla, bC = buquesCarga, bD = buquesDestructores;
             for (int i = 0; i < barcos; i++) {
@@ -83,43 +89,8 @@ int main(int argc, char *argv[]) {
                     bD--;
                 }
             }
-
-            srand(time(NULL));
-            int x, y;
-            for (int i = 0; i < barcos; i++) {
-                x = rand() % (2) + 1;
-                if (x == 1)
-                    orientacion[i] = 'V';
-                else if (x == 2)
-                    orientacion[i] = 'H';
-            }
-
-            for (int i = 0; i < barcos; i++) {
-                x = rand() % (12 + 1);
-                y = rand() % (12 + 1);
-
-                // V = ^, H = >
-                if (orientacion[i] == 'H') {
-                    if (x > 7 && y >= 0)
-                        x = x - barcosTipos[i] - 3;
-                    for (int j = 0; j < barcos; j++) {
-                        if (coordenadaSend[j][0] - 65 == x) {
-                            if (y > 0)
-                                y--;
-                            else
-                                y++;
-                        }
-                    }
-                } else if (orientacion[i] == 'V') {
-                    if (y < 6 && x >= 0)
-                        y = y + barcosTipos[i] + 3;
-                }
-
-                x = x + 65;
-                y = y + 48;
-                coordenadaSend[i][0] = x;
-                coordenadaSend[i][1] = y;
-            }
+            generarOrientacionesAleatorias(orientacion, barcos);
+            generarCoordenadasAleatorias(coordenadaSend, barcosTipos, orientacion, barcos);
 
             generarCoordenadas(tableroEnemigo, 12, 12, barcos, barcosTipos, coordenadaSend, orientacion);
             mostrarTablero(tableroEnemigo, 12, 12);
@@ -134,10 +105,110 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
+void generarOrientacionesAleatorias(char orientacion[10], int size) {
+    int x, y;
+    for (int i = 0; i < size; i++) {
+        x = rand() % (2) + 1;
+        if (x == 1)
+            orientacion[i] = 'V';
+        else if (x == 2)
+            orientacion[i] = 'H';
+    }
+}
+
+void generarCoordenadasAleatorias(char coordenadaSend[10][1], int barcosTipos[10],
+                                  char orientacion[10], int size) {
+    int x, y;
+    printf("\n");
+    for (int i = 0; i < size; i++) {
+        x = rand() % (12 + 1);
+        y = rand() % (12 + 1);
+
+        // V = ^, H = >
+        if (orientacion[i] == 'H') {
+            if (x > 7 && y >= 0)
+                x = x - barcosTipos[i] - 2;
+        }
+
+        printf("\nBarco %i: x = %i, y = %i, orientacion = %c, tipoBarco = %i", i, x, y, orientacion[i], barcosTipos[i]);
+        coordenadaSend[i][1] = y;
+        coordenadaSend[i][0] = x + 48;
+    }
+    printf("\n");
+}
+
 void reiniciarTablero(char tablero[12][12]) {
     for (int x = 0; x < 12; x++)
         for (int y = 0; y < 12; y++)
             tablero[x][y] = '*';
+}
+
+void generarCoordenadas(char tablero[12][12], int filas, int columnas, int size,
+                        int barcosTipo[10], char coordenadasInicio[10][1],
+                        char orientacion[10]) {
+    int y = 0, x = 0;
+    for (int i = 0; i < size; i++) {
+        if (coordenadasInicio[i][0] == ':')
+            x = 10;
+        else if (coordenadasInicio[i][0] == ';')
+            x = 11;
+        else if (coordenadasInicio[i][0] == '<')
+            x = 12;
+        else
+            x = (int) coordenadasInicio[i][0] - 48;
+
+        if (coordenadasInicio[i][1] == ':')
+            y = 10;
+        else if (coordenadasInicio[i][1] == ';')
+            y = 11;
+        else if (coordenadasInicio[i][1] == '<')
+            y = 12;
+        else
+            y = (int) coordenadasInicio[i][1] - 48;
+
+        printf("\nBarco %i: x = %i, y = %i, orientacion = %c, tipoBarco = %i", i, x, y, orientacion[i], barcosTipo[i]);
+        switch (barcosTipo[i]) {
+            case 1:
+                if (orientacion[i] == 'V') {
+                    tablero[x][y] = '-';
+                    tablero[x][y - 1] = '|';
+                    tablero[x][y - 2] = '|';
+                    tablero[x][y - 3] = '|';
+                    tablero[x][y - 4] = '^';
+                } else {
+                    tablero[x][y] = '|';
+                    tablero[x + 1][y] = '-';
+                    tablero[x + 2][y] = '-';
+                    tablero[x + 3][y] = '-';
+                    tablero[x + 4][y] = '>';
+                }
+                break;
+            case 2:
+                if (orientacion[i] == 'V') {
+                    tablero[x][y] = '-';
+                    tablero[x][y - 1] = '|';
+                    tablero[x][y - 2] = '|';
+                    tablero[x][y - 3] = '^';
+                } else {
+                    tablero[x][y] = '|';
+                    tablero[x + 1][y] = '-';
+                    tablero[x + 2][y] = '-';
+                    tablero[x + 3][y] = '>';
+                }
+                break;
+            case 3:
+                if (orientacion[i] == 'V') {
+                    tablero[x][y] = '-';
+                    tablero[x][y - 1] = '|';
+                    tablero[x][y - 2] = '^';
+                } else {
+                    tablero[x][y] = '|';
+                    tablero[x + 1][y] = '-';
+                    tablero[x + 2][y] = '>';
+                }
+                break;
+        }
+    }
 }
 
 void mostrarTablero(char tablero[12][12], int filas, int columnas) {
@@ -155,62 +226,4 @@ void mostrarTablero(char tablero[12][12], int filas, int columnas) {
             printf("%c  ", tablero[j][i]);
     }
     printf("\n");
-}
-
-void generarCoordenadas(char tablero[12][12], int filas, int columnas, int size,
-                        int barcosTipo[], char coordenadasInicio[12][1],
-                        char orientacion[12]) {
-    int y = 0;
-    for (int i = 0; i < size; i++) {
-        if (coordenadasInicio[i][1] == ':')
-            y = 10;
-        else if (coordenadasInicio[i][1] == ';')
-            y = 11;
-        else if (coordenadasInicio[i][1] == '<')
-            y = 12;
-        else
-            y = (int) coordenadasInicio[i][1] - 49;
-
-        switch (barcosTipo[i]) {
-            case 1:
-                if (orientacion[i] == 'V') {
-                    tablero[((int) coordenadasInicio[i][0] - 65)][y] = '-';
-                    tablero[((int) coordenadasInicio[i][0] - 65)][y - 1] = '|';
-                    tablero[((int) coordenadasInicio[i][0] - 65)][y - 2] = '|';
-                    tablero[((int) coordenadasInicio[i][0] - 65)][y - 3] = '|';
-                    tablero[((int) coordenadasInicio[i][0] - 65)][y - 4] = '^';
-                } else {
-                    tablero[((int) coordenadasInicio[i][0] - 65)][y] = '|';
-                    tablero[((int) coordenadasInicio[i][0] + 1 - 65)][y] = '-';
-                    tablero[((int) coordenadasInicio[i][0] + 2 - 65)][y] = '-';
-                    tablero[((int) coordenadasInicio[i][0] + 3 - 65)][y] = '-';
-                    tablero[((int) coordenadasInicio[i][0] + 4 - 65)][y] = '>';
-                }
-                break;
-            case 2:
-                if (orientacion[i] == 'V') {
-                    tablero[((int) coordenadasInicio[i][0] - 65)][y] = '-';
-                    tablero[((int) coordenadasInicio[i][0] - 65)][y - 1] = '|';
-                    tablero[((int) coordenadasInicio[i][0] - 65)][y - 2] = '|';
-                    tablero[((int) coordenadasInicio[i][0] - 65)][y - 3] = '^';
-                } else {
-                    tablero[((int) coordenadasInicio[i][0] - 65)][y] = '|';
-                    tablero[((int) coordenadasInicio[i][0] + 1 - 65)][y] = '-';
-                    tablero[((int) coordenadasInicio[i][0] + 2 - 65)][y] = '-';
-                    tablero[((int) coordenadasInicio[i][0] + 3 - 65)][y] = '>';
-                }
-                break;
-            case 3:
-                if (orientacion[i] == 'V') {
-                    tablero[((int) coordenadasInicio[i][0] - 65)][y] = '-';
-                    tablero[((int) coordenadasInicio[i][0] - 65)][y - 1] = '|';
-                    tablero[((int) coordenadasInicio[i][0] - 65)][y - 2] = '^';
-                } else {
-                    tablero[((int) coordenadasInicio[i][0] - 65)][y] = '|';
-                    tablero[((int) coordenadasInicio[i][0] + 1 - 65)][y] = '-';
-                    tablero[((int) coordenadasInicio[i][0] + 2 - 65)][y] = '>';
-                }
-                break;
-        }
-    }
 }
